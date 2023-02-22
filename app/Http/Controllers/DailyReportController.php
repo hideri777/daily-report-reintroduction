393f26253e4a09a601d093a545cc2e3f855ea6dd
HideakiHori <hori.hide1003@gmail.com>
2023-02-22 23:28:17 +0900

    コピー機能作った

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -46,24 +46,4 @@ class DailyReportController extends Controller
         return Redirect::route('daily_report');
     }
 
-    // /**
-    //  * Delete the user's account.
-    //  */
-    // public function destroy(Request $request): RedirectResponse
-    // {
-    //     $request->validate([
-    //         'password' => ['required', 'current-password'],
-    //     ]);
-
-    //     $user = $request->user();
-
-    //     Auth::logout();
-
-    //     $user->delete();
-
-    //     $request->session()->invalidate();
-    //     $request->session()->regenerateToken();
-
-    //     return Redirect::to('/');
-    // }
 }
--- resources/js/Pages/TodayDailyReport/Index.jsx
+++ resources/js/Pages/TodayDailyReport/Index.jsx
@@ -1,45 +1,64 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 // import UpdateDailyReportInformationForm from './Partials/UpdateDailyReportInformationForm';
 import { Head } from "@inertiajs/react";
+// import { CopyToClipboard } from "react-copy-to-clipboard";
 
 export default function Index({ auth, data }) {
-    const startDate = new Date(data.working_start);
-    const weekDay = {
-        0: '日',
-        1: '月',
-        2: '火',
-        3: '水',
-        4: '木',
-        5: '金',
-        6: '土',
-    };
-    const n = startDate.getDay();
-    const dayOfWeek = weekDay[n];
+    // const state = {value: '', copied: false};
 
-    const year = startDate.getFullYear().toString().padStart(4, '0');
-    const month = (startDate.getMonth() + 1).toString().padStart(2, '0');
-    const day = startDate.getDate().toString().padStart(2, '0');
-    const hours = startDate.getHours().toString().padStart(2, '0');
-    const minutes = startDate.getMinutes().toString().padStart(2, '0');
-    const dateText = `${year}年${month}月${day}日(${dayOfWeek}) ${hours}:${minutes}`;
+    const makeStartDate = () => {
+        const weekDay = {
+            0: '日',
+            1: '月',
+            2: '火',
+            3: '水',
+            4: '木',
+            5: '金',
+            6: '土',
+        };
+        const startDate = new Date(data.working_start);
+        const n = startDate.getDay();
+        const dayOfWeek = weekDay[n];
 
-    const endDate = new Date(data.working_end);
-    const endHours = endDate.getHours().toString().padStart(2, '0');
-    const endMinutes = endDate.getMinutes().toString().padStart(2, '0');
-    const endTime = `${endHours}:${endMinutes}`
+        const year = startDate.getFullYear().toString().padStart(4, '0');
+        const month = (startDate.getMonth() + 1).toString().padStart(2, '0');
+        const day = startDate.getDate().toString().padStart(2, '0');
+        const hours = startDate.getHours().toString().padStart(2, '0');
+        const minutes = startDate.getMinutes().toString().padStart(2, '0');
+        return `${year}年${month}月${day}日(${dayOfWeek}) ${hours}:${minutes}`;
+    }
+
+    const makeEndDate = () => {
+        const endDate = new Date(data.working_end);
+        const endHours = endDate.getHours().toString().padStart(2, '0');
+        const endMinutes = endDate.getMinutes().toString().padStart(2, '0');
+        return `${endHours}:${endMinutes}`
+    }
+
+    const copy = () => {
+        const copyText = `● 勤務時間\n${makeStartDate()} - ${makeEndDate()}\n\n● メルマガ\n${data.mail_magazine}\n\n● 本日の業務内容\n${data.this_business_content}\n\n● 次回の業務内容\n${data.next_business_content}\n\n● 本日気づいたこと・勉強になったこと・課題に感じたこと\n${data.learning}\n\n● その他\n${data.others}`;
+        navigator.clipboard.writeText(copyText)
+        .then(function() {
+            alert('コピー成功したよ');
+        }, function(error) {
+            alert('コピー失敗: ', error);
+        });
+    }
+
     return (
         <AuthenticatedLayout
             auth={auth}
             header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">日報作成</h2>}
         >
             <Head title="日報作成"/>
-
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
                     <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                        <button onClick={ () => {copy()} }>クリップボードへコピー</button>
+
                         <div className="mb-4">
                             <label htmlFor="date" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">日時</label>
-                            {dateText} ~ {endTime}
+                            {makeStartDate()} ~ {makeEndDate()}
                         </div>
                         <div className="mb-4">
                             <label htmlFor="mail_magazine" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">メルマガ</label>
